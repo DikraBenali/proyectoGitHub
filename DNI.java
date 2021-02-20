08776997ca6ca45387b56d0016acb762b3dc5901
DikraBenali <79134455+DikraBenali@users.noreply.github.com>
2021-02-20 13:16:38 +0100

    Add files via upload

---
+++ DNI.java
@@ -0,0 +1,8 @@
+
+public class DNI {
+
+    public static void main(String args[]) {
+        string[] letras={"T","R","W","A","G","M", "Y","F", "P","D",
+                         "X","B","N","J","Z","S","Q","V","H","L","C","K"," E"}
+    }
+}
---
+++ ejercicio.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class ejercicio{
+   public static void main (String[]args){
+     
+	 Scanner lector=new Scanner(System.in);
+    
+	/*
+	Codifica un programa que genere un "triángulo" como el de la figura.
+    1
+    12
+    123
+    1234
+    12345
+    123456
+    1234567
+    12345678
+    123456789
+	
+	for (int i=1;i<=9;i++){
+	
+	 
+	  for (int j=1;j<=i;j++){
+	    System.out.print(j);
+	  }
+	  System.out.println();
+    }
+	*/
+	
+    
+	/* FACTORIAL
+
+    Codifica un programa en Java que calcule el factorial de un número n:  n*(n-1)*...2*1
+    Ejemplo: el factorial de 4 (4!) será:  4*3*2*1
+	
+	 
+	System.out.println("Introduce un numero:");
+	int num = lector.nextInt();
+	int factorial=1;
+	int x=1;
+	while (num>0){
+	   factorial=factorial*num * x;
+	   num--;
+	
+	}
+	System.out.println("El factorial =" +factorial);
+	
+	*/
+	
+	
+	/*EDADES.
+    Haz un programa que pida un número al usuario (una edad de una persona) y le informe de si es menor de 18 años,
+    mayor o igual a 65, o tiene entre una edad entre 18 años y menor de 65 años.
+    
+	System.out.println("Introduce un numero:");
+	int edad= lector.nextInt();
+	
+	if (edad<=18){System.out.println("Eres menor de 18 años");}
+	else if (edad>=65){System.out.println("Eres mayor de 65 años");}
+	else {System.out.println("tienens una edad entre 18 y 65 años");}
+	*/
+	
+	
+	
+	
+	
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
